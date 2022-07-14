e7d29a4acb3cd79c99d2f5d1d53c6af074c1d09d
tominaga1653 <108568710+tominaga1653@users.noreply.github.com>
2022-07-14 11:31:29 +0000

    [Add] チェックシート35まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,55 @@
 class BooksController < ApplicationController
+
   def index
+    @user = User.find(current_user.id)
+    @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
+    @user = @book.user
+    @bookn = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    if @book.save
+      flash[:notice] = "You have created book successfully."
+      redirect_to book_path(@book.id)
+    else
+      @user = User.find(current_user.id)
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
+    @book = Book.find(params[:id])
   end
+
+  def update
+    @book = Book.find(params[:id])
+    if @book.update(book_params)
+      flash[:notice] = "You have updated book successfully."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    # flash[:notice] = "Book was successfully destroyed."
+    redirect_to books_path
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,14 @@
 class UsersController < ApplicationController
   def index
+    @user = User.find(current_user.id)
+    @book = Book.new
+    @users = User.all
   end
 
   def show
+    @user = User.find(params[:id])
+    @book = Book.new
+    @books = @user.books
   end
 
   def edit
