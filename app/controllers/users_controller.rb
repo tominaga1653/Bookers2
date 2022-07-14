73ab6bda5b9e9e23710015a5a7e338906cfe1b51
tominaga1653 <108568710+tominaga1653@users.noreply.github.com>
2022-07-14 14:01:10 +0000

    [Add] チェックシート48まで

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,5 +12,23 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(current_user.id)
   end
+
+  def update
+    @user = User.find(current_user.id)
+    if @user.update(user_params)
+      flash[:notice] = "You have updated user successfully."
+      redirect_to user_path(@user.id)
+    else
+      render :edit
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :profile_image, :introduction)
+  end
+
 end
